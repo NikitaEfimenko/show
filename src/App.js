6c3e5290f2c30e015d4506f9e3f151c81f3b650e
Nikita Efimenko <mszx2000@gmail.com>
2018-12-14 17:53:18 +0400

    kek

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Button,
+  Label,
+  Input
+} from 'reactstrap'
+
+
+const Modal = ({is, off, children}) => {
+    return (     
+         is &&  
+          <div className='my-modal my-modal_content-container'>  
+            <p className='my-modal_cross' onClick={off}> X </p>  
+            {children}
+          </div> 
+        
+    )
+} 
+
+
+const MyComp = () => {
+  return (
+      <div>
+        <Label> кто ты ? </Label>
+        <Input/>
+        <Button > ok </Button>
+      </div>
+  )
+} 
+
+
+class Counter extends Component {
+  state = {
+    count: 0
+  }
+  inc = () => {
+    this.setState({
+      count: ++this.state.count
+    })
+  }
+  dec = () => {
+    this.setState({
+      count: --this.state.count
+    })
+  }
+  render() {
+    return(
+      <div>
+        <p> {this.state.count} </p>
+        <Button onClick={this.inc}> + </Button>
+        <Button onClick={this.dec}> - </Button>
+      </div>
+     )
+  }
+}
 
 class App extends Component {
+  state = {
+    is: false
+  }
+
+  on = () => {
+    this.setState({
+      is: !this.state.is
+    })
+  }
+  off = () => {
+    this.setState({
+      is:false
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='container'>
+        <Button onClick={this.on} > polling </Button>
+        <Modal  is={this.state.is}  off={this.off}>
+          <div>
+            {[...Array(2).keys()].map(id => <Counter key={id} />)}
+            {[...Array(3).keys()].map(id => <MyComp key={id} />)}
+          </div>
+        </Modal>
       </div>
     );
   }
